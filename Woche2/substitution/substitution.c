f158d7dcf08bcf1ff45cfb8b08e8b9f992c3bf77
Koch-Dustin <dustin.koch@5minds.de>
2022-08-08 14:56:53 +0200

    added new files

---
+++ Woche2/substitution/substitution.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[]) {
+
+    if (argc != 2) {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+
+}
