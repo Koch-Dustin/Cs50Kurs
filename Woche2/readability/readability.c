6226c994304a1667ea134ec928b397caa00a2bcf
Koch-Dustin <dustin.koch@5minds.de>
2022-08-09 10:44:46 +0200

    Fixed code style

--- Woche2/readability/readability.c
+++ Woche2/readability/readability.c
@@ -1,44 +1,3 @@
-// #include <cs50.h>
-// #include <stdio.h>
-// #include <ctype.h>
-// #include <string.h>
-// #include <math.h>
-
-// int main(void) {
-
-//     string text = get_string("Text: ");
-
-//     int letters = 0;
-//     int words = 1;
-//     int sentences = 0;
-
-//     for (int i = 0; i > strlen(text); i++) {
-        
-//         if(isalpha(text[i])) {
-//             letters++;
-//         } else if(text[i] == " ") {
-//             words++;
-//         } else if(text[i] == "." || text[i] == "!" || text[i] == "?") {
-//             sentences++;
-//         }
-
-//         float L = (float) letters / (float) words * 100;
-//         float S = (float) sentences / (float) words * 100;
-//         int index = round(0.0588 * L - 0.296 * S - 15.8);
-
-//         if(index < 1) {
-//             printf("Before Grade 1");
-//         }else if(index > 16) {
-//             printf("Grade 16+");
-//         }else {
-//             printf("Grade %i", index);
-//         }
-//     }
-
-// }
-
-
-
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -47,19 +6,21 @@
 
 int main(void) {
 
-    string text = get_string("Text: ");
+    string UserText = get_string("Text: ");
 
     int letters = 0;
     int words = 1;
     int sentences = 0;
 
-    for (int i = 0; i < strlen(text); i++) {
+    for (int i = 0; i < strlen(UserText); i++) {
+        bool CharacterIsEmpty = UserText[i] == ' ';
+        bool CharacterIsSpecial = UserText[i] == '.' || UserText[i] == '!' || UserText[i] == '?';
         
-        if (isalpha(text[i])) {
+        if (isalpha(UserText[i])) {
             letters++;
-        } else if (text[i] == ' ') {
+        } else if (CharacterIsEmpty) {
             words++;
-        } else if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+        } else if (CharacterIsSpecial) {
             sentences++;
         }
 
