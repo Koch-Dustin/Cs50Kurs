cac981ee6d0fc321f99ad93ee47ed6ec0f21e957
Koch-Dustin <dustin.koch@5minds.de>
2022-08-29 09:59:49 +0200

    fixed code style

--- Woche3/tideman/tideman.c
+++ Woche3/tideman/tideman.c
@@ -91,8 +91,8 @@ bool vote(int rank, string name, int ranks[]) {
 }
 
 void record_preferences(int ranks[]) {
-    for(int i = 0; i < candidate_count; i++) {
-        for(int j = i + 1; j < candidate_count; j++) {
+    for (int i = 0; i < candidate_count; i++) {
+        for (int j = i + 1; j < candidate_count; j++) {
             preferences[ranks[i]][ranks[j]] += 1;
         }
     }
