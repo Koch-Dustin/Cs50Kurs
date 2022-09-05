ced6f927df0d133fd44c4b5e84a0601b6af6a2ba
Koch-Dustin <dustin.koch@5minds.de>
2022-09-05 09:13:24 +0200

    fixed code style

--- Woche5/inheritance/inheritance.c
+++ Woche5/inheritance/inheritance.c
@@ -15,81 +15,81 @@ const int GENERATIONS = 3;
 const int INDENT_LENGTH = 4;
 
 person *create_family(int generations);
-void print_family(person *p, int generation);
-void free_family(person *p);
+void print_family(person *personPointer, int generation);
+void free_family(person *personPointer);
 char random_allele();
 
 int main(void) {
 
     srand(time(0));
 
-    person *p = create_family(GENERATIONS);
+    person *personPointer = create_family(GENERATIONS);
 
-    print_family(p, 0);
+    print_family(personPointer, 0);
 
-    free_family(p);
+    free_family(personPointer);
 }
 
 person *create_family(int generations) {
 
-    person *p = malloc(sizeof(person));
+    person *personPointer = malloc(sizeof(person));
 
     if (generations > 1) {
-        p -> parents[0] = create_family(generations - 1);
-        p -> parents[1] = create_family(generations - 1);
+        personPointer -> parents[0] = create_family(generations - 1);
+        personPointer -> parents[1] = create_family(generations - 1);
 
-        p -> alleles[0] = p -> parents[0] -> alleles[rand() % 2];
-        p -> alleles[1] = p -> parents[1] -> alleles[rand() % 2];
+        personPointer -> alleles[0] = personPointer -> parents[0] -> alleles[rand() % 2];
+        personPointer -> alleles[1] = personPointer -> parents[1] -> alleles[rand() % 2];
 
     } else {
-        p -> parents[0] = NULL;
-        p -> parents[1] = NULL;
+        personPointer -> parents[0] = NULL;
+        personPointer -> parents[1] = NULL;
 
-        p -> alleles[0] = random_allele();
-        p -> alleles[1] = random_allele();
+        personPointer -> alleles[0] = random_allele();
+        personPointer -> alleles[1] = random_allele();
     }
 
-    return p;
+    return personPointer;
 }
 
-void free_family(person *p) {
+void free_family(person *personPointer) {
 
-    if(p == NULL) return;
+    if(personPointer == NULL) return;
 
-    free_family(p -> parents[0]);
-    free_family(p -> parents[1]);
+    free_family(personPointer -> parents[0]);
+    free_family(personPointer -> parents[1]);
 
-    free(p);
+    free(personPointer);
 }
 
-void print_family(person *p, int generation) {
-    if (p == NULL) return;
+void print_family(person *personPointer, int generation) {
+    if (personPointer == NULL) return;
 
     for (int i = 0; i < generation * INDENT_LENGTH; i++) {
         printf(" ");
     }
 
     if (generation == 0) {
-        printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Child (Generation %i): blood type %c%c\n", generation, personPointer->alleles[0], personPointer->alleles[1]);
     } else if (generation == 1) {
-        printf("Parent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Parent (Generation %i): blood type %c%c\n", generation, personPointer->alleles[0], personPointer->alleles[1]);
     } else {
         for (int i = 0; i < generation - 2; i++) {
             printf("Great-");
         }
-        printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+        printf("Grandparent (Generation %i): blood type %c%c\n", generation, personPointer->alleles[0], personPointer->alleles[1]);
     }
 
-    print_family(p->parents[0], generation + 1);
-    print_family(p->parents[1], generation + 1);
+    print_family(personPointer->parents[0], generation + 1);
+    print_family(personPointer->parents[1], generation + 1);
 }
 
 char random_allele() {
-    int r = rand() % 3;
+    int randomNumber = rand() % 3;
 
-    if (r == 0) {
+    if (randomNumber == 0) {
         return 'A';
-    } else if (r == 1) {
+    } else if (randomNumber == 1) {
         return 'B';
     } else {
         return 'O';
