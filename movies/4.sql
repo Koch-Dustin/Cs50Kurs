cea32bf26f4d44d521f066a0599b3a69f013fb3e
Dustin Koch <dustin.koch@5minds.de>
2022-09-16 08:09:47 +0200

    worked on code style

--- movies/4.sql
+++ movies/4.sql
@@ -1,1 +1,5 @@
-SELECT COUNT(title) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE rating = 10;
+SELECT COUNT(title) 
+    FROM movies 
+    JOIN ratings 
+        ON movies.id = ratings.movie_id 
+        WHERE rating = 10;
--- movies/6.sql
+++ movies/6.sql
@@ -1,1 +1,5 @@
-SELECT AVG(rating) FROM ratings JOIN movies ON movies.id = ratings.movie_id WHERE year = 2012;
+SELECT AVG(rating) 
+    FROM ratings 
+    JOIN movies 
+        ON movies.id = ratings.movie_id 
+        WHERE year = 2012;
