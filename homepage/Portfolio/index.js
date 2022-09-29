d771c2ee9d460ee000fdb94fe4be5d22e4eec937
Dustin Koch <dustin.koch@5minds.de>
2022-09-29 15:23:54 +0200

    Worked on code style

--- homepage/Portfolio/index.js
+++ homepage/Portfolio/index.js
@@ -5,42 +5,42 @@ menu3_is_open = false;
 
 function open_on_click(number) {
     num = number;
-    if(menu1_is_open) {
-        if(number == 1) {
+    if (menu1_is_open) {
+        if (number == 1) {
             document.getElementById("projekt_one_arrow").src = "img/Arrow_Right.png";
             document.getElementById("projekt_one_container").style.display = "none";
             menu1_is_open = false;
         }
-    }else {
-        if(number == 1) {
+    } else {
+        if (number == 1) {
             document.getElementById("projekt_one_arrow").src = "img/Arrow_Down.png";
             document.getElementById("projekt_one_container").style.display = "block";
             menu1_is_open = true;
         }
     }
 
-    if(menu2_is_open) {
-        if(number == 2) {
+    if (menu2_is_open) {
+        if (number == 2) {
             document.getElementById("projekt_two_arrow").src = "img/Arrow_Right.png";
             document.getElementById("projekt_two_container").style.display = "none";
             menu2_is_open = false;
         }
-    }else {
-        if(number == 2) {
+    } else {
+        if (number == 2) {
             document.getElementById("projekt_two_arrow").src = "img/Arrow_Down.png";
             document.getElementById("projekt_two_container").style.display = "block";
             menu2_is_open = true;
         }
     }
 
-    if(menu3_is_open) {
-        if(number == 3) {
+    if (menu3_is_open) {
+        if (number == 3) {
             document.getElementById("projekt_three_arrow").src = "img/Arrow_Right.png";
             document.getElementById("projekt_three_container").style.display = "none";
             menu3_is_open = false;
         }
-    }else {
-        if(number == 3) {
+    } else {
+        if (number == 3) {
             document.getElementById("projekt_three_arrow").src = "img/Arrow_Down.png";
             document.getElementById("projekt_three_container").style.display = "block";
             menu3_is_open = true;
