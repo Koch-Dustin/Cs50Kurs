5aa0c2d31a3a54c01f0c48004c8bffc919adfb65
Dustin Koch <dustin.koch@5minds.de>
2022-09-29 15:33:45 +0200

    Worked on code style

--- homepage/Portfolio/index.js
+++ homepage/Portfolio/index.js
@@ -1,50 +1,49 @@
-
 menu1_is_open = false;
 menu2_is_open = false;
 menu3_is_open = false;
 
 function open_on_click(number) {
-    num = number;
-    if (menu1_is_open) {
-        if (number == 1) {
-            document.getElementById("projekt_one_arrow").src = "img/Arrow_Right.png";
-            document.getElementById("projekt_one_container").style.display = "none";
-            menu1_is_open = false;
-        }
-    } else {
-        if (number == 1) {
-            document.getElementById("projekt_one_arrow").src = "img/Arrow_Down.png";
-            document.getElementById("projekt_one_container").style.display = "block";
-            menu1_is_open = true;
-        }
+  num = number;
+  if (menu1_is_open) {
+    if (number == 1) {
+      document.getElementById("projekt_one_arrow").src = "img/Arrow_Right.png";
+      document.getElementById("projekt_one_container").style.display = "none";
+      menu1_is_open = false;
     }
-
-    if (menu2_is_open) {
-        if (number == 2) {
-            document.getElementById("projekt_two_arrow").src = "img/Arrow_Right.png";
-            document.getElementById("projekt_two_container").style.display = "none";
-            menu2_is_open = false;
-        }
-    } else {
-        if (number == 2) {
-            document.getElementById("projekt_two_arrow").src = "img/Arrow_Down.png";
-            document.getElementById("projekt_two_container").style.display = "block";
-            menu2_is_open = true;
-        }
+  } else {
+    if (number == 1) {
+      document.getElementById("projekt_one_arrow").src = "img/Arrow_Down.png";
+      document.getElementById("projekt_one_container").style.display = "block";
+      menu1_is_open = true;
     }
+  }
 
-    if (menu3_is_open) {
-        if (number == 3) {
-            document.getElementById("projekt_three_arrow").src = "img/Arrow_Right.png";
-            document.getElementById("projekt_three_container").style.display = "none";
-            menu3_is_open = false;
-        }
-    } else {
-        if (number == 3) {
-            document.getElementById("projekt_three_arrow").src = "img/Arrow_Down.png";
-            document.getElementById("projekt_three_container").style.display = "block";
-            menu3_is_open = true;
-        }
+  if (menu2_is_open) {
+    if (number == 2) {
+      document.getElementById("projekt_two_arrow").src = "img/Arrow_Right.png";
+      document.getElementById("projekt_two_container").style.display = "none";
+      menu2_is_open = false;
     }
+  } else {
+    if (number == 2) {
+      document.getElementById("projekt_two_arrow").src = "img/Arrow_Down.png";
+      document.getElementById("projekt_two_container").style.display = "block";
+      menu2_is_open = true;
+    }
+  }
 
+  if (menu3_is_open) {
+    if (number == 3) {
+      document.getElementById("projekt_three_arrow").src = "img/Arrow_Right.png";
+      document.getElementById("projekt_three_container").style.display = "none";
+      menu3_is_open = false;
+    }
+  } else {
+    if (number == 3) {
+      document.getElementById("projekt_three_arrow").src = "img/Arrow_Down.png";
+      document.getElementById("projekt_three_container").style.display =
+        "block";
+      menu3_is_open = true;
+    }
+  }
 }
