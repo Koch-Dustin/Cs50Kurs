93c983a9459fc1cadd9b9e5121b39c00b485d89b
Dustin Koch <dustin.koch@5minds.de>
2022-10-17 10:13:38 +0200

    Worked on code style

--- homepage/Portfolio/index.js
+++ homepage/Portfolio/index.js
@@ -5,47 +5,49 @@ menu3_is_open = false;
 function open_on_click(number) {
   num = number;
   const arrow1 = document.getElementById("projekt_one_arrow");
+  const projektOneContainer = document.getElementById("projekt_one_container");
   if (menu1_is_open) {
     if (number == 1) {
       arrow1.src = "img/Arrow_Right.png"
-      document.getElementById("projekt_one_container").style.display = "none";
+      projektOneContainer.style.display = "none";
       menu1_is_open = false;
     }
   } else {
     if (number == 1) {
       arrow1.src = "img/Arrow_Down.png"
-      document.getElementById("projekt_one_container").style.display = "block";
+      projektOneContainer.style.display = "block";
       menu1_is_open = true;
     }
   }
 
   const arrow2 = document.getElementById("projekt_two_arrow");
+  const projektTwoContainer = document.getElementById("projekt_two_container");
   if (menu2_is_open) {
     if (number == 2) {
       arrow2.src = "img/Arrow_Right.png";
-      document.getElementById("projekt_two_container").style.display = "none";
+      projektTwoContainer.style.display = "none";
       menu2_is_open = false;
     }
   } else {
     if (number == 2) {
       arrow2.src = "img/Arrow_Down.png";
-      document.getElementById("projekt_two_container").style.display = "block";
+      projektTwoContainer.style.display = "block";
       menu2_is_open = true;
     }
   }
 
   const arrow3 = document.getElementById("projekt_three_arrow");
+  const projektThreeContainer = document.getElementById("projekt_three_container");
   if (menu3_is_open) {
     if (number == 3) {
       arrow3.src = "img/Arrow_Right.png";
-      document.getElementById("projekt_three_container").style.display = "none";
+      projektThreeContainer.style.display = "none";
       menu3_is_open = false;
     }
   } else {
     if (number == 3) {
       arrow3.src = "img/Arrow_Down.png";;
-      document.getElementById("projekt_three_container").style.display =
-        "block";
+      projektThreeContainer.style.display = "block";
       menu3_is_open = true;
     }
   }
